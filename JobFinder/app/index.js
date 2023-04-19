54d6e6cc9befc8da7f8976aea5d306936b02c9cd
Dilanka Seneviratne <dvseneviratne@gmail.com>
2023-04-19 17:24:41 +0530

    Simple composition
    Progress point where the composition of the application can be seen

--- JobFinder/app/index.js
+++ JobFinder/app/index.js
@@ -1,10 +1,39 @@
-import { View,Text} from 'react-native';
+import { View,Text,ScrollView,SafeAreaView} from 'react-native';
+import { useState } from 'react';
+import { Stack,useRout, useRouter } from 'expo-router';
 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 const Home = () => {
+    const router = useRouter();
+
     return(
-        <View>
-            <Text>Home </Text>
-        </View>
+        <SafeAreaView style ={{flex:1, backgroundColor :COLORS.lightWhite}}> 
+            <Stack.Screen 
+            options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible : false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+                ), 
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={images .profile} dimension="60%"/>
+                ),
+                headerTitle: "" 
+            }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}>
+                </View>
+                <Welcome/>
+                <Popularjobs/>
+                <Nearbyjobs/>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
